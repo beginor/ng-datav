d290948fd432428f1d41aba9212d598e2ee4116d
beginor <beginor@qq.com>
2024-09-05 11:15:19 +0800

    include in public api

--- projects/app-shared/src/public-api.ts
+++ projects/app-shared/src/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of app-shared
  */
 
-export * from './lib/app-shared.service';
-export * from './lib/app-shared.component';
+// export * from './lib/app-shared.service';
+// export * from './lib/app-shared.component';
+
+export * from './lib/ng-datav.module';
+export * from './lib/directives/auto-size.directive';
+export * from './lib/components/border-box-1/border-box-1.component';
