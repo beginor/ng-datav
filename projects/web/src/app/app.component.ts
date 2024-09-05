85fa09ad3371021c16ffa030c3a5458cead0b9f2
beginor <beginor@qq.com>
2024-09-05 11:16:53 +0800

    add demo for border-box-1

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BorderBox1Component } from 'app-shared'
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        BorderBox1Component,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web';
+    title = 'web';
 }
