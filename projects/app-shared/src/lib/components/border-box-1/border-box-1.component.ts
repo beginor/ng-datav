a334cdb11b125e47c4a72031ad12ecd93a1be2b9
beginor <beginor@qq.com>
2024-09-05 11:14:44 +0800

    add border-box-1

---
+++ projects/app-shared/src/lib/components/border-box-1/border-box-1.component.ts
@@ -0,0 +1,87 @@
+import { Component, input, OnInit, inject, OnDestroy, OutputRefSubscription } from '@angular/core';
+
+import { AutoSizeDirective, Size } from '../../directives/auto-size.directive';
+
+@Component({
+    selector: 'dv-border-box-1',
+    standalone: true,
+    imports: [],
+    templateUrl: './border-box-1.component.html',
+    styleUrl: './border-box-1.component.css',
+    host: { 'class': 'dv-border-box-1' },
+    hostDirectives: [AutoSizeDirective],
+})
+export class BorderBox1Component implements OnInit, OnDestroy {
+
+    public colors = input<string[]>(['#4fd2dd', '#235fa7']);
+    public backgroundColor = input<string>('transparent');
+
+    protected width = 0;
+    protected height = 0;
+
+    protected border = ['left-top', 'right-top', 'left-bottom', 'right-bottom'];
+
+    protected getBorderPoints() {
+        const width = this.width;
+        const height = this.height;
+        return [
+            `10, 27`,
+            `10, ${height - 27}`,
+            `13, ${height - 24}`,
+            `13, ${height - 21}`,
+            `24, ${height - 11}`,
+            `38, ${height - 11}`,
+            `41, ${height - 8}`,
+            `73, ${height - 8}`,
+            `75, ${height - 10}`,
+            `81, ${height - 10}`,
+            `85, ${height - 6}`,
+            `${width - 85}, ${height - 6}`,
+            `${width - 81}, ${height - 10}`,
+            `${width - 75}, ${height - 10}`,
+            `${width - 73}, ${height - 8}`,
+            `${width - 41}, ${height - 8}`,
+            `${width - 38}, ${height - 11}`,
+            `${width - 24}, ${height - 11}`,
+            `${width - 13}, ${height - 21}`,
+            `${width - 13}, ${height - 24}`,
+            `${width - 10}, ${height - 27}`,
+            `${width - 10}, 27`,
+            `${width - 13}, 25`,
+            `${width - 13}, 21`,
+            `${width - 24}, 11`,
+            `${width - 38}, 11`,
+            `${width - 41}, 8`,
+            `${width - 73}, 8`,
+            `${width - 75}, 10`,
+            `${width - 81}, 10`,
+            `${width - 85}, 6`,
+            `85, 6`,
+            `81, 10`,
+            `75, 10`,
+            `73, 8`,
+            `41, 8`,
+            `38, 11`,
+            `24, 11`,
+            `13, 21`,
+            `13, 24`
+        ].join(' ');
+    }
+
+    private autoSize = inject(AutoSizeDirective, { self: true });
+    private autoSize$?: OutputRefSubscription;
+
+    public ngOnInit() {
+        this.autoSize$ = this.autoSize.sizeChanged.subscribe(
+            size => { this.onSizeChanged(size); });
+    }
+
+    public ngOnDestroy(): void {
+        this.autoSize$?.unsubscribe();
+    }
+
+    public onSizeChanged({ width, height }: Size) {
+        this.width = width;
+        this.height = height;
+    }
+}
