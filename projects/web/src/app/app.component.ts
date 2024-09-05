1764e2b5a454d18542d5ce3ce46237fa4560fc22
beginor <beginor@qq.com>
2024-09-05 14:32:29 +0800

    add border box 2 demo

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { BorderBox1Component } from 'app-shared'
+import { NgDataVModule } from 'app-shared';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
         RouterOutlet,
-        BorderBox1Component,
+        NgDataVModule,
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+
     title = 'web';
+
 }
